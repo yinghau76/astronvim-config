0cdd049351850c68e4032d1b6edf1fb50e4c3e35
Patrick Tsai <yingtsai@amazon.com>
2023-06-07 02:05:55 -0700

    options: Use CaskaydiaCove Nerd Font

--- options.lua
+++ options.lua
@@ -7,7 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
-    guifont = "Delugia:h15",
+    -- guifont = "Delugia:h15",
+    guifont = "CaskaydiaCove Nerd Font:h15",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
